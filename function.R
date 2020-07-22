3c1215360632c511a572638d7788ff4155c529df
Arian Maghsoudnia <arian.m95@gmail.com>
2020-07-22 16:58:45 +0200

    R weather update 22July

--- function.R
+++ function.R
@@ -1,3 +1,5 @@
+rm(list = ls(all.names = TRUE))
+
 if (!require("rio")) install.packages("rio")
 install_formats()
 if (!require("RNCEP")) install.packages("RNCEP") 
@@ -20,8 +22,17 @@ mk_path(directory,fileName,fileType)
 
 months <- c(1,2)
 years <- c(2018,2018)
-latitude <- c(45,46)
-longtitude <- c(9,10)
+lat_0 <- 45
+lat_1 <- 47.5
+lon_0 <- 7.5
+lon_1 <- 10
+latitude <- c(lat_0,lat_1)
+longtitude <- c(lon_0,lon_1)
+time_step <-6
+
+exact_lat <- 45.46
+exact_lon <- 9.19
+
 ## Retrieve the temperature from a particular pressure level for
 ## a specified spatial and temporal extent
 north_it_t <- NCEP.gather(variable='air.sig995', level='surface',
@@ -82,29 +93,123 @@ head(df_2,10)
 #               cols=heat.colors(64), transparency=.5, axis.args=NULL, map.args=NULL,
 #               grid.args=NULL, title.args=NULL, interp.loess.args=NULL,
 #               image.plot.args=NULL, contour.args=NULL, points.args=NULL)
+
+##Function to drop unwanted columns
 df_drop_col <- function(df,col2drp)
 {
   df <- df[, !(names(df) %in% col2drp)]
   return(df)
 }
-std_date_time <- function(chr)
+
+##Function to drop unwanted row
+# df_drop_col <- function(df,row2drp)
+# {
+#   df <- df[-row2drp,]
+#   return(df)
+# }
+
+##Function to replace "_" with "-" in the datetime column retrieved from 
+##RNCEP package to make it more like the standrad R date format
+
+rename_std <- function(chr)
 {
   chr <- gsub("_","-",chr)
   return(chr)
 }
 
+##As we are dealing with large dataframes, these simple functions helps us to 
+##print faster a useful brief first and last portion of any large dataframe
 hd <- function(df)
 {
   return(head(df,10))
 }
 
-apply(north_it_df_t[],1,std_date_time)
-north_it_df_t[ ,"datetime"] <- apply(as.array(north_it_df_t[ ,"datetime"]),1,std_date_time)
+tl <- function(df)
+{
+  return(tail(df,10))
+}
 
-std_date_time(north_it_df_t)
-df_temp <- north_it_df_t
-df_temp$date<- substr(df_temp$datetime,1,10)
-df_temp$date<- as.Date(df_temp$date)
-df_temp$time<- substr(df_temp$datetime,nchar(df_temp$datetime)-1,nchar(df_temp$datetime))
-df_temp$date<- as.Date(df_temp$date)
-typeof(df_temp[1,5])
+##Function to change the datetime format of the df and separate it
+##to a standard date and a time row
+std_date_time <- function(df)
+{  
+  df[ ,"datetime"] <- apply(as.array(df[ ,"datetime"]),1,rename_std)
+  df$date<- substr(df$datetime,1,10)
+  df$date<- as.Date(df$date)
+  df$time<- substr(df$datetime,nchar(df$datetime)-1,nchar(df$datetime))
+  df$time<- as.integer(df$time)
+  return(df)
+}
+
+## The function gets a dataframe and shifts the colummn "col2shift"
+## by "shiftval" rows
+shift_col <- function(df,col2shift,shift_val)
+{
+  df_shifted<-df
+  seq_df=as.integer(seq(1,nrow(df),1))
+  counter<-0
+  for(i in seq_df)
+  {
+    df_shifted[[col2shift]][i] <- df[[col2shift]][i+shift_val]
+    counter<-counter+1
+  }
+  return(df_shifted)
+}
+
+df_temp <- std_date_time(north_it_df_t)
+###Shift the longitude by one two compare and see if we are 
+df_shifted_lon <- shift_col(df_temp,"longitude",1)
+df_temp$shifted_lon <- df_shifted_lon$longitude
+###Checks if two executive rows belong to the same longitude
+df_temp$same_lat <- df_temp$longitude == df_temp$shifted_lon
+###Dividing the df which is labeled as True or False; true if the longitude 
+###is the same as the next row; eventually diving the df to two dataframes each
+### one for one set of latitude numbers. True/False sequence does not matter.
+df_false <- df_temp[df_temp$same_lat==TRUE, ]
+df_true <- df_temp[df_temp$same_lat==FALSE, ]
+### To substract two executive temperatures for interpolation function 
+df_false$shifted_t <-  (shift_col(df_false,"temperature",1))$temperature
+df_true$shifted_t <-  (shift_col(df_true,"temperature",1))$temperature
+
+df_false$shifted_time <-  (shift_col(df_false,"time",1))$time
+df_true$shifted_time <-  (shift_col(df_true,"time",1))$time
+
+df_false$time_change_coeff <- ((df_false$time-df_false$shifted_time))
+df_true$time_change_coeff <- ((df_true$time-df_true$shifted_time))
+
+
+cte_long <- (exact_lon-lon_0)/(lon_1-lon_0)
+df_false$t_interp_lon <- (cte_long*df_false$shifted_t + (1-cte_long)*df_false$temperature)
+df_true$t_interp_lon <- (cte_long*df_true$shifted_t + (1-cte_long)*df_true$temperature)
+
+df_false$exact_lon <- exact_lon
+df_true$exact_lon <- exact_lon
+
+df_false_red <- df_false[df_false$time_change_coeff==0, ]
+df_true_red <- df_true[df_true$time_change_coeff==0, ]
+
+todrop <- c("shifted_lat","shifted_lon","same_lat","shifted_t","shifted_time","time_change_coeff","longitude")
+
+df_false_red <- df_drop_col(df_false_red,todrop)
+df_true_red <- df_drop_col(df_true_red,todrop)
+
+indices_lat_0 <- which(df_temp$latitude==lat_0)
+indices_lon_0 <- which(df_temp$longitude==lon_0)
+df_temp_lat <- df_temp[indices_lon_0,]
+
+df_joined <- df_false_red
+###levelize df_true and df_false to combine
+df_true_red[nrow(df_false_red),] <- NA
+### Now we can interpolate btw the latitudes
+lat_false <- df_false[1,2]
+lat_true <- df_true[1,2]
+lat_step <- lat_false-lat_true
+cte_lat <- (lat_false-exact_lat)/lat_step
+df_joined$temp_interp <- cte_lat*df_true_red$t_interp_lon+(1-cte_lat)*df_false_red$t_interp_lon
+
+df_clean <- df_joined
+clean_drop <- c("datetime","latitude","temperature","t_interp_lon","exact_lon")
+df_clean <- df_drop_col(df_clean,clean_drop)
+hd(df_clean)
+
+
